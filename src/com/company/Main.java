52f8574ce17fec4292c4674c6c882d01d7b725a6
Aleksandr <zayander@gmail.com>
2017-03-16 14:12:04 +0200

    init

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int age = 18;
+	double height = 1.8;
+	String name = "Коле";
+	System.out.println(name + " уже " + age + " лет, и он ростом  " + height + " метра.");
+    }
+}
