9625a8bd75c0110d9f1b3c65b20b56e950c966c3
Aleksandr <zayander@gmail.com>
2017-03-21 14:37:26 +0200

    boolean lesson

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,73 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	int age = 18;
-	double height = 1.8;
-	String name = "Коле";
-	System.out.println(name + " уже " + age + " лет, и он ростом  " + height + " метра.");
-    }
+public static  void main(String[] args) {
+
+	int ageMisha = 41;
+	int ageVadim = 32;
+	int ageVasya = 23;
+
+if(ageMisha < ageVadim && ageMisha < ageVasya) {
+
+	if (ageVadim < ageVasya) {
+		System.out.println("oldest man - " + ageVasya);
+		System.out.println("middle man - " + ageVadim);
+		System.out.println("youngest man - " + ageMisha);
+
+	}
+	if (ageVasya < ageVadim) {
+		System.out.println("oldest man - " + ageVadim);
+		System.out.println("middle man- " + ageVasya);
+		System.out.println("youngest man - " + ageMisha);
+
+	}
+
+
 }
+
+
+	if(ageVadim < ageVasya && ageVadim < ageMisha) {
+
+		if (ageVasya < ageMisha) {
+			System.out.println("oldest  man - " + ageMisha);
+			System.out.println("middle man - " + ageVasya);
+			System.out.println("youngest man - " + ageVadim);
+
+		}
+		if (ageMisha < ageVasya) {
+			System.out.println("oldest man - " + ageVasya);
+			System.out.println("middle man - " + ageMisha);
+			System.out.println("youngest man - " + ageVadim);
+
+		}
+
+
+	}
+
+
+	if(ageVasya < ageVadim && ageVasya < ageMisha) {
+
+		if (ageVadim < ageMisha) {
+			System.out.println("oldest man - " + ageMisha);
+			System.out.println("middle man - " + ageVadim);
+			System.out.println("youngest man- " + ageVasya);
+
+		}
+		if (ageMisha < ageVadim) {
+			System.out.println("oldest man - " + ageVadim);
+			System.out.println("middle man- " + ageMisha);
+			System.out.println("youngest man - " + ageVasya);
+
+		}
+
+
+	}
+
+
+
+
+
+}
+}
+
+
